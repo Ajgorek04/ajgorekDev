1d64573340352d0ece3bef8f39daac0ca1af3c14
Ajgorek <igorblendzinski12@gmail.com>
2023-07-25 00:41:49 +0200

    solving bugs related to animation errors in safarii

--- scripts/hamburgerMenu.js
+++ scripts/hamburgerMenu.js
@@ -1,8 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
+const mainAnimateText = document.querySelectorAll(".contentHeader span");
+const arrowdownAnimate = document.querySelector(".arrowdown");
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("open-menu");
+
+  // Wyłaczenie animacji po otwarciu hamburger menu (powoduje bug na safari)
+  mainAnimateText.forEach((span) => {
+    span.classList.toggle("span-hider");
+  });
+  arrowdownAnimate.classList.toggle("scrollDownHider");
 });
 
 // Chowanie po nacisnieciu na jakis link
