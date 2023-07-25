0cdb95ce595c6ee4203da7d83090cb1ee93b1ff9
Ajgorek <igorblendzinski12@gmail.com>
2023-07-25 19:36:05 +0200

    InterSectionObserver I make new mechanic to make observer, now it doesnt have any bugs, it works fine, just like I expected. In this commit I only make 1 observer for about me

--- scripts/InterSectionObserver.js
+++ scripts/InterSectionObserver.js
@@ -1,16 +1,33 @@
-const wiem = document.querySelectorAll(".anim");
+const infoDiv = document.querySelectorAll(".info");
+const me_info = document.querySelector(".me_info");
+const technologies_info = document.querySelector(".technologies_info");
 
-observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.intersectionRatio > 0) {
-      // Forwards - wlasciwosc animation-fill-mode sprawia ze element zachowa stan animacji
-      entry.target.style.animation = "anim1 1s forwards ease-out";
-    } else {
-      entry.target.style.animation = "none";
+if (windowWidth > 1200) {
+  observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio > 1 / 3) {
+          // Adjust the threshold here /\
+          // Dostosuj wartość progu tutaj /\
+          me_info.classList.add("offAnim");
+          technologies_info.classList.add("offAnim");
+        } else {
+          me_info.classList.remove("offAnim");
+          technologies_info.classList.remove("offAnim");
+        }
+      });
+    },
+    {
+      threshold: 1 / 3, // Set the threshold to 1/3 (approximately 33.33%)
+      // Ustaw próg na 1/3 około 33,33%
     }
-  });
-});
+  );
+} else if (windowWidth <= 1200) {
+  console.log("wiem");
+}
 
-wiem.forEach((item) => {
-  observer.observe(item);
+infoDiv.forEach((item) => {
+  if (windowWidth > 1200) {
+    observer.observe(item);
+  }
 });
