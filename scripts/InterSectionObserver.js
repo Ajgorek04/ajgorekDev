e7688a95c087e862edc5fbf46deecde7d4676e00
Ajgorek <igorblendzinski12@gmail.com>
2023-07-26 15:26:37 +0200

    Fixed problem with observer name I just defined wrong name

--- scripts/InterSectionObserver.js
+++ scripts/InterSectionObserver.js
@@ -1,25 +1,33 @@
+const aboutme_header = document.querySelector(".aboutme_header");
 const infoDiv = document.querySelectorAll(".info");
 const me_info = document.querySelector(".me_info");
 const technologies_info = document.querySelector(".technologies_info");
+const goDown = document.querySelector(".goDown");
+
+// SECTION ABOUT ME
 
 if (windowWidth > 1200) {
   observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
-        if (entry.intersectionRatio > 1 / 3) {
+        if (entry.intersectionRatio > 1 / 4) {
           // Adjust the threshold here /\
           // Dostosuj wartość progu tutaj /\
           me_info.classList.add("offAnim");
           technologies_info.classList.add("offAnim");
+          setInterval(() => {
+            goDown.classList.add("offAnim");
+          }, 1000);
         } else {
           me_info.classList.remove("offAnim");
           technologies_info.classList.remove("offAnim");
+          goDown.classList.remove("offAnim");
         }
       });
     },
     {
-      threshold: 1 / 3, // Set the threshold to 1/3 (approximately 33.33%)
-      // Ustaw próg na 1/3 około 33,33%
+      threshold: 1 / 4, // Set the threshold to 1/4
+      // Ustaw próg na 1/4
     }
   );
 } else if (windowWidth <= 1200) {
@@ -31,3 +39,5 @@ infoDiv.forEach((item) => {
     observer.observe(item);
   }
 });
+
+// END SECTION ABOUT ME
